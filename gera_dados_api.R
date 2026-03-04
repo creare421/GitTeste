ad146f87af9b2dd09a6792d6ec64b21280e9b35d
creare421 <edimeire.a.pinto@tesouro.gov.br>
2026-03-04 20:33:42 -0300

    Subi pela primeira vez os códigos

---
+++ gera_dados_api.R
@@ -0,0 +1,52 @@
+
+library(httr)
+library(jsonlite)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+
+base_url_rreo <- "apidatalake.tesouro.gov.br/ords/siconfi/tt/rreo?"
+
+# parâmetros de consulata ao RREO
+ano <- "2024"
+bimestre <- "1"
+tipo_relatorio <- "RREO"
+num_anexo <- "RREO-Anexo+01"
+ente <- c("3304557", "3127701","3115409")
+
+
+
+chamada<-function(ente){
+  chamada_api_rreo <- paste(base_url_rreo,
+                            "an_exercicio=", ano, "&",
+                            "nr_periodo=", bimestre, "&",
+                            "co_tipo_demonstrativo=", tipo_relatorio, "&",
+                            "no_anexo=", num_anexo, "&",
+                            "id_ente=", ente, sep = "")
+  
+  rreo <- GET(chamada_api_rreo)
+  
+  
+  status_code(rreo)   ### 200 é ok
+  
+  rreo_txt <- content(rreo, as="text", encoding="UTF-8")
+  
+  rreo_json <- fromJSON(rreo_txt, flatten = FALSE) 
+  
+  
+  rreo_df <- as.data.frame(rreo_json[["items"]]) 
+  
+  
+  base<-knitr::kable(head(rreo_df))
+  
+  return(rreo_df )
+  
+}
+
+
+res<-map_dfr(1:3,~chamada(ente[.x]))
+
+res
+
+View(res)
